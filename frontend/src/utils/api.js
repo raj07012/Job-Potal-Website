2a247516ab5eb499b22e9132cd31db28b89dd2e1
raj07012 <140056567+raj07012@users.noreply.github.com>
2026-03-31 16:29:00 +0530

    again updated the baseURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({ baseURL: '/api' });
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 api.interceptors.request.use((config) => {
